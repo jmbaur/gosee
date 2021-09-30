ca8c5914ff8db9993a68663447d46bffd364d99c
Jared Baur <jaredbaur@fastmail.com>
2021-09-30 14:09:25 -0700

    Split out build and derivation

--- default.nix
+++ default.nix
@@ -1,6 +1,2 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.buildGoModule {
-  name = "gosee";
-  src = builtins.path { path = ./.; };
-  vendorSha256 = "17icimk6gnhxbv0dc5h9ld6v9ksq63i5zhh0yv5j7hh43j84abb8";
-}
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.callPackage ./derivation.nix { }
---
+++ derivation.nix
@@ -0,0 +1,6 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.buildGoModule {
+  name = "gosee";
+  src = builtins.path { path = ./.; };
+  vendorSha256 = "17icimk6gnhxbv0dc5h9ld6v9ksq63i5zhh0yv5j7hh43j84abb8";
+}
