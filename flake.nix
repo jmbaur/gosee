c85777aa567f9fd39068a0019b1c2262f3635908
Jared Baur <jaredbaur@fastmail.com>
2021-12-29 08:35:24 +0000

    Use CGO_ENABLED=0

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
         packages.gosee = pkgs.buildGoModule {
           name = "gosee";
           src = builtins.path { path = ./.; };
+          CGO_ENABLED = 0;
           vendorSha256 = "17icimk6gnhxbv0dc5h9ld6v9ksq63i5zhh0yv5j7hh43j84abb8";
         };
         defaultPackage = packages.gosee;
