5101fc91d20ebd89883e6e31aa17c24b1b655c3a
Jared Baur <jaredbaur@fastmail.com>
2022-01-27 18:31:28 -0800

    Add flake app

--- flake.nix
+++ flake.nix
@@ -4,22 +4,31 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }@inputs:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        rec {
-          devShell = pkgs.mkShell { buildInputs = with pkgs; [ git go entr ]; };
-          packages.gosee = pkgs.buildGoModule {
-            name = "gosee";
-            src = builtins.path { path = ./.; };
-            CGO_ENABLED = 0;
-            vendorSha256 = "17icimk6gnhxbv0dc5h9ld6v9ksq63i5zhh0yv5j7hh43j84abb8";
-          };
-          defaultPackage = packages.gosee;
-        })
-    //
-    {
-      overlay = self: super: { gosee = inputs.self.packages.gosee; };
+  outputs = { self, nixpkgs, flake-utils }@inputs: {
+    overlay = final: prev: {
+      gosee = nixpkgs.legacyPackages.${prev.system}.buildGo117Module {
+        pname = "gosee";
+        version = "0.1.0";
+        src = builtins.path { path = ./.; };
+        CGO_ENABLED = 0;
+        vendorSha256 = "17icimk6gnhxbv0dc5h9ld6v9ksq63i5zhh0yv5j7hh43j84abb8";
+      };
     };
+  } //
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        overlays = [ self.overlay ];
+        inherit system;
+      };
+    in
+    rec {
+      devShell = pkgs.mkShell { buildInputs = with pkgs; [ git go_1_17 entr ]; };
+      packages.gosee = pkgs.gosee;
+      defaultPackage = pkgs.gosee;
+      apps.gosee = flake-utils.lib.mkApp { drv = pkgs.gosee; name = "gosee"; };
+      defaultApp = apps.gosee;
+    });
+
+
 }
