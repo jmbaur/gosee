d3811b640b035a0ae8a3925e25e728ba9923ceee
Jared Baur <jaredbaur@fastmail.com>
2022-01-27 18:19:57 -0800

    Fix overlay

--- flake.nix
+++ flake.nix
@@ -1,21 +1,25 @@
 {
   description = "gosee";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.11-small";
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [ git go entr ]; };
-        packages.gosee = pkgs.buildGoModule {
-          name = "gosee";
-          src = builtins.path { path = ./.; };
-          CGO_ENABLED = 0;
-          vendorSha256 = "17icimk6gnhxbv0dc5h9ld6v9ksq63i5zhh0yv5j7hh43j84abb8";
-        };
-        defaultPackage = packages.gosee;
-        overlay = self: super: { gosee = packages.gosee; };
-      });
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        rec {
+          devShell = pkgs.mkShell { buildInputs = with pkgs; [ git go entr ]; };
+          packages.gosee = pkgs.buildGoModule {
+            name = "gosee";
+            src = builtins.path { path = ./.; };
+            CGO_ENABLED = 0;
+            vendorSha256 = "17icimk6gnhxbv0dc5h9ld6v9ksq63i5zhh0yv5j7hh43j84abb8";
+          };
+          defaultPackage = packages.gosee;
+        })
+    //
+    {
+      overlay = self: super: { gosee = self.packages.gosee; };
+    };
 }
