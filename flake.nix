d441a88814dd91cc2e698954191e05c7cf545b47
Jared Baur <jaredbaur@fastmail.com>
2022-08-03 13:11:21 -0700

    Nix flake update

--- flake.nix
+++ flake.nix
@@ -4,27 +4,26 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }@inputs: {
+  outputs = inputs: with inputs; {
     overlays.default = final: prev: {
-      gosee = nixpkgs.legacyPackages.${prev.system}.buildGo117Module {
+      gosee = prev.buildGoModule {
         pname = "gosee";
         version = "0.1.0";
-        src = builtins.path { path = ./.; };
+        src = ./.;
         CGO_ENABLED = 0;
-        vendorSha256 = "sha256-ISlKGEdypPxKUB7eht4Wj+zLdTA1z1tPvBE4vsVaEyU=";
+        vendorSha256 = "sha256-0pmE22lo4mxYuAluCnXTliNVLacGxAMLmmr7W5ex+uI=";
       };
     };
-  } //
-  flake-utils.lib.eachDefaultSystem (system:
+  } // flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
-        overlays = [ self.overlays.default ];
         inherit system;
+        overlays = [ self.overlays.default ];
       };
     in
     rec {
       devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ git go_1_18 entr ];
+        buildInputs = with pkgs; [ go_1_18 entr ];
         CGO_ENABLED = 0;
       };
       packages.gosee = pkgs.gosee;
--- go.mod
+++ go.mod
@@ -5,12 +5,12 @@ go 1.17
 require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/radovskyb/watcher v1.0.7
-	github.com/yuin/goldmark v1.4.11
+	github.com/yuin/goldmark v1.4.13
 	github.com/yuin/goldmark-highlighting v0.0.0-20220208100518-594be1970594
 )
 
 require (
 	github.com/alecthomas/chroma v0.10.0 // indirect
 	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
-	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/dlclark/regexp2 v1.7.0 // indirect
 )
