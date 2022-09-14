89f0aaac3cc78b03786a062d32a08adfe6249496
Jared Baur <jaredbaur@fastmail.com>
2022-09-14 13:19:19 -0700

    Update deps

--- default.nix
+++ default.nix
@@ -1,17 +1,17 @@
 { buildGoModule, writeShellScriptBin }:
 let
-  gosee = buildGoModule {
+  drv = buildGoModule {
     pname = "gosee";
     version = "0.1.0";
     src = ./.;
     CGO_ENABLED = 0;
     vendorSha256 = "sha256-Zd0YRadV8Gfy2dzP2b9nqZQsR4rXedu+1IHEoYDuzmQ=";
     passthru.update = writeShellScriptBin "update" ''
-      if [[ $(${gosee.go}/bin/go get -u ./...) != "" ]]; then
-        sed -i 's/vendorSha256\ =\ "sha256-.*";/vendorSha256="sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";/' default.nix
-        echo "run 'nix build' then update the vendorSha256 field with the correct value"
+      if [[ $(${drv.go}/bin/go get -u all 2>&1) != "" ]]; then
+        sed -i 's/vendorSha256\ =.*;/vendorSha256="sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";/' default.nix
+        ${drv.go}/bin/go mod tidy
       fi
     '';
   };
 in
-gosee
+drv
