f45facf5c5fc0b8bf6dbcd51da984747225b639f
Jared Baur <jaredbaur@fastmail.com>
2022-09-25 12:30:06 -0700

    Fix vendorSha256

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let
     pname = "gosee";
     version = "0.2.0";
     src = ./.;
-    vendorSha256 = "sha256-guuJ5NZk7+Ya+oRcvg2paDIagEpKSrcqYJwoorCmb4U=";
+    vendorSha256 = "sha256-gi1GUaus3r/3P8KBdWndEmHxAXg6vPXnQysGBezO0rQ=";
     # this cannot be a symlink since go:embed will not read symlinks
     preBuild = ''
       if [[ ! -f static/github-markdown.css ]]; then
