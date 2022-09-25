6fe72115a45e02dd766b2972808362b5a80afb62
Jared Baur <jaredbaur@fastmail.com>
2022-09-25 12:49:51 -0700

    Don't remove all content on closed connection

--- static/index.js
+++ static/index.js
@@ -1,10 +1,8 @@
 var inner = document.getElementById("inner");
 var conn = new WebSocket(`ws://${location.host}/ws`);
 conn.onclose = function (evt) {
-  inner.innerHTML = "Connection closed";
-  console.log("connection closed");
+  alert("connection closed");
 };
 conn.onmessage = function (evt) {
-  console.log("file updated");
   inner.innerHTML = evt.data;
 };
