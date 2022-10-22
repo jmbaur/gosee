62626016ca6d7cbdd26a63ca245972bb760f0b97
Jared Baur <jaredbaur@fastmail.com>
2022-10-22 08:55:31 -0700

    Nix flake update

--- default.nix
+++ default.nix
@@ -1,21 +1,14 @@
-{ buildGoModule
-, github-markdown-css
-, CGO_ENABLED
-, ...
-}:
-let
-  drv = buildGoModule {
-    pname = "gosee";
-    version = "0.2.1";
-    src = ./.;
-    vendorSha256 = "sha256-gi1GUaus3r/3P8KBdWndEmHxAXg6vPXnQysGBezO0rQ=";
-    # this cannot be a symlink since go:embed will not read symlinks
-    preBuild = ''
-      if [[ ! -f static/github-markdown.css ]]; then
-        cp ${github-markdown-css}/github-markdown.css static/github-markdown.css
-      fi
-    '';
-    inherit CGO_ENABLED;
-  };
-in
-drv
+{ buildGoModule, github-markdown-css, CGO_ENABLED, ... }:
+buildGoModule {
+  pname = "gosee";
+  version = "0.2.1";
+  src = ./.;
+  vendorSha256 = "sha256-gi1GUaus3r/3P8KBdWndEmHxAXg6vPXnQysGBezO0rQ=";
+  # this cannot be a symlink since go:embed will not read symlinks
+  preBuild = ''
+    if [[ ! -f static/github-markdown.css ]]; then
+      cp ${github-markdown-css}/github-markdown.css static/github-markdown.css
+    fi
+  '';
+  inherit CGO_ENABLED;
+}
