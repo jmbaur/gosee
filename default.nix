019c65a27b9bb37f14d3f144d67446197a441d1a
Jared Baur <jaredbaur@fastmail.com>
2022-11-03 01:58:25 -0700

    Version bump

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { buildGoModule, CGO_ENABLED ? 0, ... }:
 buildGoModule {
   pname = "gosee";
-  version = "0.2.1";
+  version = "0.2.2";
   src = ./.;
   vendorSha256 = "sha256-gi1GUaus3r/3P8KBdWndEmHxAXg6vPXnQysGBezO0rQ=";
   ldflags = [ "-s" "-w" ];
