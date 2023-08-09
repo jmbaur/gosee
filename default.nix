a7bb184a33b23ea662fe1fc76dab0e78e1a8cc00
Jared Baur <jaredbaur@fastmail.com>
2023-08-09 08:47:04 -0700

    Update inputs

--- default.nix
+++ default.nix
@@ -3,6 +3,6 @@ buildGoModule {
   pname = "gosee";
   version = "0.2.2";
   src = ./.;
-  vendorSha256 = "sha256-/EVloZRF/YGSejuNhl9um4tZiwqZ3tMTkaM84wCVrkQ=";
+  vendorSha256 = "sha256-e79IpLAMCHllSgg9w8tWn4d/Zv+WVDPPjOEUF3+3LFc=";
   ldflags = [ "-s" "-w" ];
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.18
 require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/radovskyb/watcher v1.0.7
-	github.com/yuin/goldmark v1.5.4
+	github.com/yuin/goldmark v1.5.5
 )
