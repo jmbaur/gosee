851d56659d1f52fc3c39e88bb3f015a1239ec720
Jared Baur <jaredbaur@fastmail.com>
2023-11-17 06:48:32 -0800

    Use vendorHash

--- default.nix
+++ default.nix
@@ -3,6 +3,6 @@ buildGoModule {
   pname = "gosee";
   version = "0.2.2";
   src = ./.;
-  vendorSha256 = "sha256-EUlqmPOl8rshRdeQQElba/XCX21W9m2wSvYZFFMwiUk=";
+  vendorHash = "sha256-EUlqmPOl8rshRdeQQElba/XCX21W9m2wSvYZFFMwiUk=";
   ldflags = [ "-s" "-w" ];
 }
