7b3f4bc125a915fc34dd55a21291419bfadf1ca5
jmbaur <45740526+jmbaur@users.noreply.github.com>
2024-12-01 01:04:30 +0000

    Update dependencies

--- default.nix
+++ default.nix
@@ -3,6 +3,6 @@ buildGoModule {
   pname = "gosee";
   version = "0.2.2";
   src = ./.;
-  vendorHash = "sha256-+47rPmgWVmL3+xYbZsaCGmBB7X5LD3k6gBz5/6ltxNU=";
+  vendorHash = "";
   ldflags = [ "-s" "-w" ];
 }
